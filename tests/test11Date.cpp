f2f60d5da7e71516ff4aaa30b921c0945dfc021c
deshortone <jingshianggu@gmail.com>
2024-03-06 19:57:17 +0000

    test(date)

---
+++ tests/test11Date.cpp
@@ -0,0 +1,40 @@
+// -----------------------------------------------------
+// CSC371 Advanced Object Oriented Programming (2023/24)
+// Department of Computer Science, Swansea University
+//
+// Author: Thomas Reitmaier
+//         thomas.reitmaier@swansea.ac.uk
+//         Martin Porcheron (adapted from)
+//         m.a.w.porcheron@swansea.ac.uk
+//
+// Canvas: https://canvas.swansea.ac.uk/courses/44636
+// -----------------------------------------------------
+// Catch2 — https://github.com/catchorg/Catch2
+// Catch2 is licensed under the BOOST license
+// -----------------------------------------------------
+// This file contains tests for the action program
+// argument.
+// -----------------------------------------------------
+
+#include "../src/lib_catch.hpp"
+
+#include <string>
+
+#include "../src/lib_cxxopts.hpp"
+#include "../src/lib_cxxopts_argv.hpp"
+
+#include "../src/371todo.h"
+
+SCENARIO("Date")
+{
+    Date date1{"2024-03-06"};
+    date1.setDate(1111, 11, 11);
+    Date date2{"1111-11-11"};
+    REQUIRE(date1 == date2);
+}
+
+SCENARIO("Leap year")
+{
+    Date date1{};
+    REQUIRE(date1.isValidDate(2024, 2, 29));
+}
