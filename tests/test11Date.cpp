79811c2525fc6cca1e55b90ff8a38bf69646f76d
deshortone <jingshianggu@gmail.com>
2024-03-09 23:32:54 +0000

    test(date): maur

--- tests/test11Date.cpp
+++ tests/test11Date.cpp
@@ -28,7 +28,10 @@
 SCENARIO("Date")
 {
     Date date1{"2024-03-06"};
+    REQUIRE(date1.str() == "2024-3-6");
     date1.setDate(1111, 11, 11);
+    REQUIRE(date1.str() == "1111-11-11");
+
     Date date2{"1111-11-11"};
     REQUIRE(date1 == date2);
 }
@@ -37,4 +40,31 @@ SCENARIO("Leap year")
 {
     Date date1{};
     REQUIRE(date1.isValidDate(2024, 2, 29));
+    REQUIRE_FALSE(date1.isValidDate(2023, 2, 29));
+    REQUIRE_FALSE(date1.isValidDate(2022, 2, 29));
+    REQUIRE(date1.isValidDate(2028, 2, 29));
+}
+
+SCENARIO("Less than operator")
+{
+    Date date1{};
+    Date date2{};
+
+    REQUIRE(date1.str() == "");
+    REQUIRE_FALSE(date1 < date2);
+
+    date1.setDateFromString("2024-3-9");
+    date2.setDateFromString("2024-3-10");
+    REQUIRE(date1 < date2);
+    REQUIRE_FALSE(date2 < date1);
+
+    date1.setDateFromString("2024-2-9");
+    date2.setDateFromString("2024-3-9");
+    REQUIRE(date1 < date2);
+    REQUIRE_FALSE(date2 < date1);
+
+    date1.setDateFromString("2022-3-9");
+    date2.setDateFromString("2024-3-9");
+    REQUIRE(date1 < date2);
+    REQUIRE_FALSE(date2 < date1);
 }
