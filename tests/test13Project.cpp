786c82dbc2bc70e9a4ccb7128c2d0b885e22aac7
deshortone <jingshianggu@gmail.com>
2024-03-10 09:53:39 +0000

    test(project): more

--- tests/test13Project.cpp
+++ tests/test13Project.cpp
@@ -34,10 +34,10 @@ SCENARIO("Merge 2 tasks with same identifier")
     task1.addTag("d");
 
     Task task2{"task1"};
-    task1.addTag("c");
-    task1.addTag("d");
-    task1.addTag("e");
-    task1.addTag("f");
+    task2.addTag("c");
+    task2.addTag("d");
+    task2.addTag("e");
+    task2.addTag("f");
 
     Task taskAnswer{"task1"};
     taskAnswer.addTag("a");
@@ -53,4 +53,43 @@ SCENARIO("Merge 2 tasks with same identifier")
 
     REQUIRE(project.size() == 1);
     REQUIRE(project.getTask("task1") == taskAnswer);
+}
+
+SCENARIO("add tasks with different identifiers")
+{
+    Task task1{"task1"};
+    task1.addTag("a");
+    task1.addTag("b");
+
+    Task task2{"task2"};
+    task2.addTag("c");
+    task2.addTag("d");
+
+    Task task3{"task3"};
+
+    Project project{"new Project"};
+    project.addTask(task1);
+    project.addTask(task2);
+
+    REQUIRE(project.size() == 2);
+    REQUIRE(project.getTask("task1") == task1);
+    REQUIRE(project.getTask("task2") == task2);
+
+    std::vector<Task> allProjects{task1, task2};
+    REQUIRE(project.getTasks() == allProjects);
+
+    REQUIRE(project.newTask("task1") == task1);
+    REQUIRE(project.newTask("task2") == task2);
+    REQUIRE(project.newTask("task3") == task3); // Added task here!
+    REQUIRE(project.findTask("task2").base()->str() == task2.str());
+    REQUIRE(project.containsTask("task1"));
+    REQUIRE(project.containsTask("task2"));
+
+    REQUIRE_THROWS_AS(project.deleteTask("bad"), NoTaskError);
+    REQUIRE_THROWS_WITH(project.deleteTask("bad"), "unknown task with identifier 'bad'");
+    REQUIRE(project.size() == 3);
+    REQUIRE(project.deleteTask("task1"));
+    REQUIRE(project.size() == 2);
+    REQUIRE_FALSE(project.containsTask("task1"));
+    REQUIRE(project.containsTask("task2"));
 }
