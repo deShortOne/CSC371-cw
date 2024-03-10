0b93f1d8e86dd2e3735de42b61e26c3343da64a1
deshortone <jingshianggu@gmail.com>
2024-03-10 09:32:40 +0000

    test(task): more

--- tests/test12Task.cpp
+++ tests/test12Task.cpp
@@ -25,21 +25,22 @@
 
 #include "../src/371todo.h"
 
-SCENARIO("")
+SCENARIO("Date in task")
 {
     Task task1{"task1"};
-
-    std::cout << task1.getDueDate().str();
 
     Date date1{"2024-03-06"};
     task1.setDueDate(date1);
     REQUIRE(date1 == task1.getDueDate());
+    REQUIRE(task1.getDueDate().str() == "2024-3-6");
 
     Date date2{"1111-11-11"};
     task1.getDueDate().setDate(1111, 11, 11);
-    std::cout << task1.getDueDate().str();
+    REQUIRE(task1.getDueDate().str() == "1111-11-11");
 
     REQUIRE(date2 == task1.getDueDate());
+
+    REQUIRE(task1.str() == "{\"completed\":false,\"dueDate\":\"1111-11-11\",\"tags\":[]}");
 } // SCENARIO
 
 SCENARIO("tag editing")
@@ -61,4 +62,50 @@ SCENARIO("tag editing")
 
     REQUIRE(task1.numTags() == 3);
     REQUIRE_FALSE(task1.containsTag("a"));
+
+    REQUIRE_FALSE(task1.addTag("b"));
+
+    std::vector<std::string> listOfTags{"b", "c", "d"};
+    REQUIRE(task1.getTags() == listOfTags);
+
+    REQUIRE(task1.str() == "{\"completed\":false,\"dueDate\":\"\",\"tags\":[\"b\",\"c\",\"d\"]}");
+}
+
+SCENARIO("tag comparison")
+{
+    Task task1{"task1"};
+    task1.addTag("a");
+    task1.addTag("b");
+
+    Task task1Alt{"task1"};
+    task1Alt.addTag("a");
+    task1Alt.addTag("b");
+
+    Task task2{"task2"};
+    task2.addTag("a");
+    task2.addTag("b");
+
+    Task task3{"task3"};
+    task3.addTag("1");
+    task3.addTag("b");
+
+    Task task4{"task4"};
+    task4.addTag("1");
+    task4.addTag("2");
+
+    Task task5{"task1"};
+    task4.addTag("1");
+    task4.addTag("2");
+
+    Task task6{"task1"};
+    task4.addTag("a");
+    task4.addTag("2");
+
+    REQUIRE(task1 == task1);
+    REQUIRE(task1 == task1Alt);
+    REQUIRE_FALSE(task1 == task2);
+    REQUIRE_FALSE(task1 == task3);
+    REQUIRE_FALSE(task1 == task4);
+    REQUIRE_FALSE(task1 == task5);
+    REQUIRE_FALSE(task1 == task6);
 }
